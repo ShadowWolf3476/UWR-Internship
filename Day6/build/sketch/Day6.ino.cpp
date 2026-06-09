3731194e4404c7e35f8c5fbf66aad23b7b5b1525
ShadowWolf3476 <shadowwolf3476@gmail.com>
2026-06-09 11:18:43 +0530

    Day7_Firstcommit

--- Day6/build/sketch/Day6.ino.cpp
+++ Day6/build/sketch/Day6.ino.cpp
@@ -1,3 +1,10 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\hp\\UWR-Internship\\Day6\\Day6.ino"
+#line 1 "C:\\Users\\hp\\UWR-Internship\\Day6\\Day6.ino"
+void setup();
+#line 8 "C:\\Users\\hp\\UWR-Internship\\Day6\\Day6.ino"
+void loop();
+#line 1 "C:\\Users\\hp\\UWR-Internship\\Day6\\Day6.ino"
 void setup()
 {
 	pinMode(A0,INPUT);
@@ -15,5 +22,6 @@ void loop()
     }else{
         digitalWrite(13,LOW);
     }
-    delay(100);
+    delay(1000);
 }
+
